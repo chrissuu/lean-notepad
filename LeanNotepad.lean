930727c135ea40a6ce1eb6a1e54012ce5ff3a8cf
Chris Su <chrisssu19@gmail.com>
2025-10-01 14:16:26 -0400

    Boiler lean setup code

---
+++ LeanNotepad.lean
@@ -0,0 +1,4 @@
+-- This module serves as the root of the `LeanNotepad` library.
+-- Import modules here that should be built as part of the library.
+import «LeanNotepad».Basic
+import «group-theory».group
